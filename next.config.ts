74fdd41b1f159f011a1c05928c748e246dfd03d2
Hadi Montazeran <84393450+hadimhm2000@users.noreply.github.com>
2026-07-31 20:40:50 +0330

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  serverExternalPackages: ["sharp"],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
+  },
+};
 
 export default nextConfig;
